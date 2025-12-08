b21739a86bef7a4a35a2c8be7a87a49e0c3360c8
andrei <servici.2022@gmail.com>
2025-12-08 16:31:31 +0200

    Fix: Update humanoid_gazebo package with new launch file and configurations

--- src/simulation/humanoid_gazebo/launch/gazebo.launch.py
+++ src/simulation/humanoid_gazebo/launch/gazebo.launch.py
@@ -16,6 +16,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_prefix
 
 
@@ -114,15 +115,18 @@ def generate_launch_description():
     ])
 
     # Get URDF via xacro
-    humanoid_leg_description_content = Command([
-        PathJoinSubstitution([FindExecutable(name='xacro')]),
-        ' ',
-        urdf_file,
-        ' use_sim:=',
-        use_sim,
-        ' prefix:=',
-        prefix
-    ])
+    humanoid_leg_description_content = ParameterValue(
+        Command([
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            urdf_file,
+            ' use_sim:=',
+            use_sim,
+            ' prefix:=',
+            prefix
+        ]),
+        value_type=str
+    )
 
     robot_description = {'robot_description': humanoid_leg_description_content}
 
